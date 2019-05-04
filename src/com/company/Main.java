bcf298cdb5e7384cd8a1ea03cb2b9aa94e656eb1
aluno02 sala04 <aluno02sala04@targettrust.local>
2019-05-04 17:07:13 -0300

    incluindo classes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,20 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(nome);
+        Nota nota1 = new Nota();
+        nota1.setValor(10.0);
+        Nota nota2 = new Nota();
+        nota2.setValor(6.5);
+
+        Double media = calcularMediaDasNotas(nota1,nota2);
+
+    }
+
+    public static Double calcularMediaDasNotas(Nota nota1, Nota nota2){
+
+       Double resultado = (nota1.getValor() + nota2.getValor()) / 2;
+        return resultado;
     }
 }
+
+
---
+++ src/com/company/Nota.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Nota {
+    Double valor;
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+}
