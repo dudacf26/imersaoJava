7fc17fd7a84c7f7772db5074469a64a78961cec0
Alexandre Viegas <aluno02sala04@targettrust.local>
2019-06-29 17:02:35 -0300

    adicionando aula08

---
+++ aula08/DataBaseConfig.java
@@ -0,0 +1,10 @@
+@Configuration
+@PropertySource({ "classpath:application.properties" })
+public class DatabaseConfig {
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+}
